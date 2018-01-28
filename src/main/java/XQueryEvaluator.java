a861de2863b9be802a60bea5d0c2c3e39c18a669
WenbinZhu <zwbhsefz@gmail.com>
2018-01-28 15:23:22 -0800

    Finish XQueryEvaluator, added xml file oupupt codes

--- src/main/java/XQueryEvaluator.java
+++ src/main/java/XQueryEvaluator.java
@@ -4,24 +4,35 @@ import main.antlr.XQueryLexer;
 import main.antlr.XQueryParser;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class XQueryEvaluator {
 
+    @SuppressWarnings("deprecated")
     public static void main(String[] args) throws Exception {
-        if (args.length == 0) {
-            System.out.println("Input query file must be provided.");
+        if (args.length != 2) {
+            System.out.println("Input query file and output file must be provided.");
             System.exit(1);
         }
 
         // Execute input query
         String queryFile = args[0];
+        String outputFile = args[1];
         FileInputStream fis = new FileInputStream(queryFile);
         ANTLRInputStream ais = new ANTLRInputStream(fis);
         XQueryLexer lexer = new XQueryLexer(ais);
@@ -31,7 +42,27 @@ public class XQueryEvaluator {
         XQueryEvalVisitor visitor = new XQueryEvalVisitor();
         List<Node> queryResult = visitor.visit(tree);
 
-        // Write query result to xml file
+        // Generate output xml object
         System.out.println("Query result: " + queryResult);
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        // Add query result nodes to xml object
+        Node resultNode = doc.createElement("Result");
+        for (Node node : queryResult) {
+            Node copy = doc.importNode(node, true);
+            resultNode.appendChild(copy);
+        }
+        doc.appendChild(resultNode);
+
+        // Generate output xml file
+        TransformerFactory tfFactory = TransformerFactory.newInstance();
+        Transformer transformer = tfFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new FileOutputStream(outputFile));
+        transformer.transform(source, result);
     }
 }
